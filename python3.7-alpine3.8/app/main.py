416953b67e5e2f294a6e3e8833d81fe5647497d0
Sebastián Ramírez <tiangolo@gmail.com>
2019-01-12 17:45:00 +0400

    :sparkles: Add images for Python 3.6, 3.7, standard or Alpine

---
+++ python3.7-alpine3.8/app/main.py
@@ -0,0 +1,14 @@
+import sys
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    version = "{}.{}".format(sys.version_info.major, sys.version_info.minor)
+    message = "Hello World from Flask in a Docker container running Python {} with Meinheld and Gunicorn on Alpine (default)".format(
+        version
+    )
+    return message
